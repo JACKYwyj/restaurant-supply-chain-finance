5980386f39c18a7c8234b67c51da5575e4724848
Jacky Wang <wangyunjie@icloud.com>
2026-03-08 01:59:56 +0800

    Add Apple-style dashboard and Electron packaging

---
+++ electron/main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1400,
+    height: 900,
+    minWidth: 1024,
+    minHeight: 700,
+    title: '餐饮供应链金融平台',
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+  win.loadFile(path.join(__dirname, '../frontend/index.html'));
+  win.webContents.openDevTools();
+}
+
+app.whenReady().then(createWindow);
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
